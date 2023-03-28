797b9e131174567c56315ce6eda2f29dc0de3891
Matias Moncada <matias.monc95@outlook.com>
2023-03-28 18:48:19 -0300

    se crea api

---
+++ veterinaria-back/veterinaria-back/Controllers/AgendaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace veterinaria_back.Controllers
+{
+    [ApiController]
+    [Route("api/agenda")]
+    public class AgendaController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<respuestaAgenda> Agendar(InputAgenda datosAgenda)
+        {
+            respuestaAgenda respuesta = new respuestaAgenda();
+            if(datosAgenda != null)
+            {
+                respuesta.codigo = 1;
+                respuesta.respuesta = "Consulta agendada exitosamente";
+            }
+
+            return respuesta;
+        }
+    }
+
+    public class respuestaAgenda
+    {
+        public int codigo { get; set; }
+        public string respuesta { get; set; }
+    }
+
+    public class InputAgenda
+    {
+        public string fecha { get; set; }
+        public string hora { get; set; }
+        public string direccion { get; set; }
+        public string atencion { get; set; }
+    }
+}
